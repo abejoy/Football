286c17d5c46ddc37eccd2c2cf42e7f3cf83b5d15
abejoy <abrahamjoys98@gmail.com>
2019-11-27 16:07:42 +0000

    debugging tests bulllshit6

--- Football.cpp
+++ Football.cpp
@@ -132,7 +132,7 @@ int Midfielder::getScore() const {
     std::cout<< "i go this after getting const " << genericScore << endl;
     //goals points
     genericScore += GOAL_POINT * getGoalsScored();
-    std::cout<< "i go this after getting goal scored " << genericScore << endl;
+    std::cout<< "i go this after getting goal scored " << GOAL_POINT << " " << getGoalsScored()  << endl;
 
     //conceded points
     genericScore += (Player::getTeam()->getGoalsConceded() == 0) ? NO_GOALS_CONCEDED_POINT : 0;
